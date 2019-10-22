16aa77ae8351f1768ba055f4b2dc514442fd6f46
EsportsGrandpa <matthiasboeckaert@gmail.com>
2019-10-22 14:26:47 +0200

    hamburger menu ok

--- persoonlijke website/script.js
+++ persoonlijke website/script.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
-    $(".hamburger").on("click", function () {
+    $(".hamburger").on("click", function (e) {
+        e.preventDefault();
         $(".navbar-side").toggleClass("active");
     });
     $(".close").on("click", function () {
         $(".navbar-side").toggleClass("active");
     });
+
+    function Close() {
+        //alert('close');
+        $(".navbar-side").toggleClass("active");
+    }
+
+    $('.navbar-side a').on('click', Close);
 });
